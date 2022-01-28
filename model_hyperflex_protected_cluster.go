8ae4a90ec84df96cbcd74b96888797c603f34431
Vikrant Balyan <vvb@users.noreply.github.com>
2022-01-28 11:22:25 +0530

    refreshing sdk for intersight version 5208 (#4)

--- model_hyperflex_protected_cluster.go
+++ model_hyperflex_protected_cluster.go
@@ -3,7 +3,7 @@ Cisco Intersight
 
 Cisco Intersight is a management platform delivered as a service with embedded analytics for your Cisco and 3rd party IT infrastructure. This platform offers an intelligent level of management that enables IT organizations to analyze, simplify, and automate their environments in more advanced ways than the prior generations of tools. Cisco Intersight provides an integrated and intuitive management experience for resources in the traditional data center as well as at the edge. With flexible deployment options to address complex security needs, getting started with Intersight is quick and easy. Cisco Intersight has deep integration with Cisco UCS and HyperFlex systems allowing for remote deployment, configuration, and ongoing maintenance. The model-based deployment works for a single system in a remote location or hundreds of systems in a data center and enables rapid, standardized configuration and deployment. It also streamlines maintaining those systems whether you are working with small or very large configurations. The Intersight OpenAPI document defines the complete set of properties that are returned in the HTTP response. From that perspective, a client can expect that no additional properties are returned, unless these properties are explicitly defined in the OpenAPI document. However, when a client uses an older version of the Intersight OpenAPI document, the server may send additional properties because the software is more recent than the client. In that case, the client may receive properties that it does not know about. Some generated SDKs perform a strict validation of the HTTP response body against the OpenAPI document.
 
-API version: 1.0.9-4950
+API version: 1.0.9-5208
 Contact: intersight@cisco.com
 */
 
@@ -40,6 +40,7 @@ type HyperflexProtectedCluster struct {
 	TargetDatastoreName *string `json:"TargetDatastoreName,omitempty"`
 	// Percent usage of the datastore.
 	TargetDatastoreUtilization *float32 `json:"TargetDatastoreUtilization,omitempty"`
+	BackupPolicy *HyperflexClusterBackupPolicyInventoryRelationship `json:"BackupPolicy,omitempty"`
 	DatastoreStatistic *HyperflexDatastoreStatisticRelationship `json:"DatastoreStatistic,omitempty"`
 	SrcCluster *HyperflexClusterRelationship `json:"SrcCluster,omitempty"`
 	TgtCluster *HyperflexClusterRelationship `json:"TgtCluster,omitempty"`
@@ -375,6 +376,38 @@ func (o *HyperflexProtectedCluster) SetTargetDatastoreUtilization(v float32) {
 	o.TargetDatastoreUtilization = &v
 }
 
+// GetBackupPolicy returns the BackupPolicy field value if set, zero value otherwise.
+func (o *HyperflexProtectedCluster) GetBackupPolicy() HyperflexClusterBackupPolicyInventoryRelationship {
+	if o == nil || o.BackupPolicy == nil {
+		var ret HyperflexClusterBackupPolicyInventoryRelationship
+		return ret
+	}
+	return *o.BackupPolicy
+}
+
+// GetBackupPolicyOk returns a tuple with the BackupPolicy field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *HyperflexProtectedCluster) GetBackupPolicyOk() (*HyperflexClusterBackupPolicyInventoryRelationship, bool) {
+	if o == nil || o.BackupPolicy == nil {
+		return nil, false
+	}
+	return o.BackupPolicy, true
+}
+
+// HasBackupPolicy returns a boolean if a field has been set.
+func (o *HyperflexProtectedCluster) HasBackupPolicy() bool {
+	if o != nil && o.BackupPolicy != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetBackupPolicy gets a reference to the given HyperflexClusterBackupPolicyInventoryRelationship and assigns it to the BackupPolicy field.
+func (o *HyperflexProtectedCluster) SetBackupPolicy(v HyperflexClusterBackupPolicyInventoryRelationship) {
+	o.BackupPolicy = &v
+}
+
 // GetDatastoreStatistic returns the DatastoreStatistic field value if set, zero value otherwise.
 func (o *HyperflexProtectedCluster) GetDatastoreStatistic() HyperflexDatastoreStatisticRelationship {
 	if o == nil || o.DatastoreStatistic == nil {
@@ -511,6 +544,9 @@ func (o HyperflexProtectedCluster) MarshalJSON() ([]byte, error) {
 	if o.TargetDatastoreUtilization != nil {
 		toSerialize["TargetDatastoreUtilization"] = o.TargetDatastoreUtilization
 	}
+	if o.BackupPolicy != nil {
+		toSerialize["BackupPolicy"] = o.BackupPolicy
+	}
 	if o.DatastoreStatistic != nil {
 		toSerialize["DatastoreStatistic"] = o.DatastoreStatistic
 	}
@@ -550,6 +586,7 @@ func (o *HyperflexProtectedCluster) UnmarshalJSON(bytes []byte) (err error) {
 		TargetDatastoreName *string `json:"TargetDatastoreName,omitempty"`
 		// Percent usage of the datastore.
 		TargetDatastoreUtilization *float32 `json:"TargetDatastoreUtilization,omitempty"`
+		BackupPolicy *HyperflexClusterBackupPolicyInventoryRelationship `json:"BackupPolicy,omitempty"`
 		DatastoreStatistic *HyperflexDatastoreStatisticRelationship `json:"DatastoreStatistic,omitempty"`
 		SrcCluster *HyperflexClusterRelationship `json:"SrcCluster,omitempty"`
 		TgtCluster *HyperflexClusterRelationship `json:"TgtCluster,omitempty"`
@@ -570,6 +607,7 @@ func (o *HyperflexProtectedCluster) UnmarshalJSON(bytes []byte) (err error) {
 		varHyperflexProtectedCluster.TargetClusterName = varHyperflexProtectedClusterWithoutEmbeddedStruct.TargetClusterName
 		varHyperflexProtectedCluster.TargetDatastoreName = varHyperflexProtectedClusterWithoutEmbeddedStruct.TargetDatastoreName
 		varHyperflexProtectedCluster.TargetDatastoreUtilization = varHyperflexProtectedClusterWithoutEmbeddedStruct.TargetDatastoreUtilization
+		varHyperflexProtectedCluster.BackupPolicy = varHyperflexProtectedClusterWithoutEmbeddedStruct.BackupPolicy
 		varHyperflexProtectedCluster.DatastoreStatistic = varHyperflexProtectedClusterWithoutEmbeddedStruct.DatastoreStatistic
 		varHyperflexProtectedCluster.SrcCluster = varHyperflexProtectedClusterWithoutEmbeddedStruct.SrcCluster
 		varHyperflexProtectedCluster.TgtCluster = varHyperflexProtectedClusterWithoutEmbeddedStruct.TgtCluster
@@ -600,6 +638,7 @@ func (o *HyperflexProtectedCluster) UnmarshalJSON(bytes []byte) (err error) {
 		delete(additionalProperties, "TargetClusterName")
 		delete(additionalProperties, "TargetDatastoreName")
 		delete(additionalProperties, "TargetDatastoreUtilization")
+		delete(additionalProperties, "BackupPolicy")
 		delete(additionalProperties, "DatastoreStatistic")
 		delete(additionalProperties, "SrcCluster")
 		delete(additionalProperties, "TgtCluster")
--- model_storage_net_app_cluster.go
+++ model_storage_net_app_cluster.go
@@ -3,7 +3,7 @@ Cisco Intersight
 
 Cisco Intersight is a management platform delivered as a service with embedded analytics for your Cisco and 3rd party IT infrastructure. This platform offers an intelligent level of management that enables IT organizations to analyze, simplify, and automate their environments in more advanced ways than the prior generations of tools. Cisco Intersight provides an integrated and intuitive management experience for resources in the traditional data center as well as at the edge. With flexible deployment options to address complex security needs, getting started with Intersight is quick and easy. Cisco Intersight has deep integration with Cisco UCS and HyperFlex systems allowing for remote deployment, configuration, and ongoing maintenance. The model-based deployment works for a single system in a remote location or hundreds of systems in a data center and enables rapid, standardized configuration and deployment. It also streamlines maintaining those systems whether you are working with small or very large configurations. The Intersight OpenAPI document defines the complete set of properties that are returned in the HTTP response. From that perspective, a client can expect that no additional properties are returned, unless these properties are explicitly defined in the OpenAPI document. However, when a client uses an older version of the Intersight OpenAPI document, the server may send additional properties because the software is more recent than the client. In that case, the client may receive properties that it does not know about. Some generated SDKs perform a strict validation of the HTTP response body against the OpenAPI document.
 
-API version: 1.0.9-4950
+API version: 1.0.9-5208
 Contact: intersight@cisco.com
 */
 
@@ -26,7 +26,7 @@ type StorageNetAppCluster struct {
 	ObjectType string `json:"ObjectType"`
 	AvgPerformanceMetrics *StorageNetAppPerformanceMetricsAverage `json:"AvgPerformanceMetrics,omitempty"`
 	ClusterEfficiency NullableStorageNetAppStorageClusterEfficiency `json:"ClusterEfficiency,omitempty"`
-	// The health status of the cluster. Possible states are ok, ok-with-suppressed, degraded, and unreachable.
+	// The health status of the cluster. Possible states are ok, ok-with-suppressed, degraded, and unreachable. * `Unreachable` - Cluster status is unreachable. * `OK` - Cluster status is either ok or ok-with-suppressed. * `Degraded` - Cluster status is degraded.
 	ClusterHealthStatus *string `json:"ClusterHealthStatus,omitempty"`
 	DnsDomains []string `json:"DnsDomains,omitempty"`
 	// Unique identifier of NetApp Cluster across data center.
@@ -37,6 +37,8 @@ type StorageNetAppCluster struct {
 	ManagementAddress *string `json:"ManagementAddress,omitempty"`
 	NameServers []string `json:"NameServers,omitempty"`
 	NtpServers []string `json:"NtpServers,omitempty"`
+	// An array of relationships to storageNetAppClusterEvent resources.
+	Events []StorageNetAppClusterEventRelationship `json:"Events,omitempty"`
 	RegisteredDevice *AssetDeviceRegistrationRelationship `json:"RegisteredDevice,omitempty"`
 	AdditionalProperties map[string]interface{}
 }
@@ -415,6 +417,39 @@ func (o *StorageNetAppCluster) SetNtpServers(v []string) {
 	o.NtpServers = v
 }
 
+// GetEvents returns the Events field value if set, zero value otherwise (both if not set or set to explicit null).
+func (o *StorageNetAppCluster) GetEvents() []StorageNetAppClusterEventRelationship {
+	if o == nil  {
+		var ret []StorageNetAppClusterEventRelationship
+		return ret
+	}
+	return o.Events
+}
+
+// GetEventsOk returns a tuple with the Events field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+// NOTE: If the value is an explicit nil, `nil, true` will be returned
+func (o *StorageNetAppCluster) GetEventsOk() (*[]StorageNetAppClusterEventRelationship, bool) {
+	if o == nil || o.Events == nil {
+		return nil, false
+	}
+	return &o.Events, true
+}
+
+// HasEvents returns a boolean if a field has been set.
+func (o *StorageNetAppCluster) HasEvents() bool {
+	if o != nil && o.Events != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetEvents gets a reference to the given []StorageNetAppClusterEventRelationship and assigns it to the Events field.
+func (o *StorageNetAppCluster) SetEvents(v []StorageNetAppClusterEventRelationship) {
+	o.Events = v
+}
+
 // GetRegisteredDevice returns the RegisteredDevice field value if set, zero value otherwise.
 func (o *StorageNetAppCluster) GetRegisteredDevice() AssetDeviceRegistrationRelationship {
 	if o == nil || o.RegisteredDevice == nil {
@@ -490,6 +525,9 @@ func (o StorageNetAppCluster) MarshalJSON() ([]byte, error) {
 	if o.NtpServers != nil {
 		toSerialize["NtpServers"] = o.NtpServers
 	}
+	if o.Events != nil {
+		toSerialize["Events"] = o.Events
+	}
 	if o.RegisteredDevice != nil {
 		toSerialize["RegisteredDevice"] = o.RegisteredDevice
 	}
@@ -509,7 +547,7 @@ func (o *StorageNetAppCluster) UnmarshalJSON(bytes []byte) (err error) {
 		ObjectType string `json:"ObjectType"`
 		AvgPerformanceMetrics *StorageNetAppPerformanceMetricsAverage `json:"AvgPerformanceMetrics,omitempty"`
 		ClusterEfficiency NullableStorageNetAppStorageClusterEfficiency `json:"ClusterEfficiency,omitempty"`
-		// The health status of the cluster. Possible states are ok, ok-with-suppressed, degraded, and unreachable.
+		// The health status of the cluster. Possible states are ok, ok-with-suppressed, degraded, and unreachable. * `Unreachable` - Cluster status is unreachable. * `OK` - Cluster status is either ok or ok-with-suppressed. * `Degraded` - Cluster status is degraded.
 		ClusterHealthStatus *string `json:"ClusterHealthStatus,omitempty"`
 		DnsDomains []string `json:"DnsDomains,omitempty"`
 		// Unique identifier of NetApp Cluster across data center.
@@ -520,6 +558,8 @@ func (o *StorageNetAppCluster) UnmarshalJSON(bytes []byte) (err error) {
 		ManagementAddress *string `json:"ManagementAddress,omitempty"`
 		NameServers []string `json:"NameServers,omitempty"`
 		NtpServers []string `json:"NtpServers,omitempty"`
+		// An array of relationships to storageNetAppClusterEvent resources.
+		Events []StorageNetAppClusterEventRelationship `json:"Events,omitempty"`
 		RegisteredDevice *AssetDeviceRegistrationRelationship `json:"RegisteredDevice,omitempty"`
 	}
 
@@ -539,6 +579,7 @@ func (o *StorageNetAppCluster) UnmarshalJSON(bytes []byte) (err error) {
 		varStorageNetAppCluster.ManagementAddress = varStorageNetAppClusterWithoutEmbeddedStruct.ManagementAddress
 		varStorageNetAppCluster.NameServers = varStorageNetAppClusterWithoutEmbeddedStruct.NameServers
 		varStorageNetAppCluster.NtpServers = varStorageNetAppClusterWithoutEmbeddedStruct.NtpServers
+		varStorageNetAppCluster.Events = varStorageNetAppClusterWithoutEmbeddedStruct.Events
 		varStorageNetAppCluster.RegisteredDevice = varStorageNetAppClusterWithoutEmbeddedStruct.RegisteredDevice
 		*o = StorageNetAppCluster(varStorageNetAppCluster)
 	} else {
@@ -568,6 +609,7 @@ func (o *StorageNetAppCluster) UnmarshalJSON(bytes []byte) (err error) {
 		delete(additionalProperties, "ManagementAddress")
 		delete(additionalProperties, "NameServers")
 		delete(additionalProperties, "NtpServers")
+		delete(additionalProperties, "Events")
 		delete(additionalProperties, "RegisteredDevice")
 
 		// remove fields from embedded structs
