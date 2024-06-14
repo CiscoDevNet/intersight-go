6df725bb4d26c3a8772d17da9492da842d37e02f
Vikrant Balyan <vvb@cisco.com>
2024-06-14 13:14:35 +0530

    refreshing sdk for intersight version 17227

--- test/api_appliance_test.go
+++ test/api_appliance_test.go
@@ -1234,6 +1234,32 @@ func Test_intersight_ApplianceApiService(t *testing.T) {
 
 	})
 
+	t.Run("Test ApplianceApiService GetApplianceUpgradeTrackerByMoid", func(t *testing.T) {
+
+		t.Skip("skip test") // remove to run test
+
+		var moid string
+
+		resp, httpRes, err := apiClient.ApplianceApi.GetApplianceUpgradeTrackerByMoid(context.Background(), moid).Execute()
+
+		require.Nil(t, err)
+		require.NotNil(t, resp)
+		assert.Equal(t, 200, httpRes.StatusCode)
+
+	})
+
+	t.Run("Test ApplianceApiService GetApplianceUpgradeTrackerList", func(t *testing.T) {
+
+		t.Skip("skip test") // remove to run test
+
+		resp, httpRes, err := apiClient.ApplianceApi.GetApplianceUpgradeTrackerList(context.Background()).Execute()
+
+		require.Nil(t, err)
+		require.NotNil(t, resp)
+		assert.Equal(t, 200, httpRes.StatusCode)
+
+	})
+
 	t.Run("Test ApplianceApiService PatchApplianceAutoRmaPolicy", func(t *testing.T) {
 
 		t.Skip("skip test") // remove to run test
